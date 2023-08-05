a814a55b29d87ef621d16df37799edfd150c3ab1
Abdoulaye33 <Abdulaibayo08@outlook.fr>
2023-08-05 04:25:09 +0200

    basic php api code done but not working cause of the directory autoload.php not loading

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,39 @@
-<?php
-declare(strict_types=1);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Advice Generator App</title>
+</head>
+<body>
 
-echo 'It works !';
+    <?php 
+
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+
+        use GuzzleHttp\Client;
+
+        require '../vendor/autoload.php';
+
+
+
+        $client = new Client();
+        $response = $client->get('https://api.adviceslip.com/advice');
+        $data = json_decode($response->getBody(), true);
+
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
+
+        if (isset($data['slip']['advice'])) {
+            echo '<h2>' . $data['slip']['advice'] . '</h2>';
+        } else {
+            echo 'Aucune donnée disponible';
+        }
+
+    ?>
+
+</body>
+</html>
